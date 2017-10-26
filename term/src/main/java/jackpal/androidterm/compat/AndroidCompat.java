b5384d43af179bb13f7746553c4cd9b983b0a1c7
Roumen Petrov <maintainer@termoneplus.com>
2017-10-26 20:17:39 +0300

    remove work-around for VERSION.SDK_INT (android >= 4) as current minimum is 9

--- term/src/main/java/jackpal/androidterm/compat/AndroidCompat.java
+++ term/src/main/java/jackpal/androidterm/compat/AndroidCompat.java
@@ -14,34 +14,12 @@ package jackpal.androidterm.compat;
  */
 
 public class AndroidCompat {
-    public final static int SDK = getSDK();
+    public final static int SDK = android.os.Build.VERSION.SDK_INT;
 
     // The era of Holo Design
     public final static boolean V11ToV20;
 
     static {
         V11ToV20 = (SDK >= 11) && (SDK <= 20);
-    }
-
-    private final static int getSDK() {
-        int result;
-        try {
-            result = AndroidLevel4PlusCompat.getSDKInt();
-        } catch (VerifyError e) {
-            // We must be at an SDK level less than 4.
-            try {
-                result = Integer.valueOf(android.os.Build.VERSION.SDK);
-            } catch (NumberFormatException e2) {
-                // Couldn't parse string, assume the worst.
-                result = 1;
-            }
-        }
-        return result;
-    }
-}
-
-class AndroidLevel4PlusCompat {
-    static int getSDKInt() {
-        return android.os.Build.VERSION.SDK_INT;
     }
 }
