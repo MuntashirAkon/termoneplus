c94cb5a819fa9afa9d1258f3d49705bf3925b475
Roumen Petrov <maintainer@termoneplus.com>
2017-11-02 22:12:44 +0200

    emulatorview: remove work-around for VERSION.SDK_INT (android >= 4) as current minimum is 9

--- emulatorview/src/main/java/jackpal/androidterm/emulatorview/compat/AndroidCompat.java
+++ emulatorview/src/main/java/jackpal/androidterm/emulatorview/compat/AndroidCompat.java
@@ -14,27 +14,5 @@ package jackpal.androidterm.emulatorview.compat;
  */
 
 public class AndroidCompat {
-    public final static int SDK = getSDK();
-
-    private final static int getSDK() {
-        int result;
-        try {
-            result = AndroidLevel4PlusCompat.getSDKInt();
-        } catch (VerifyError e) {
-            // We must be at an SDK level less than 4.
-            try {
-                result = Integer.valueOf(android.os.Build.VERSION.SDK);
-            } catch (NumberFormatException e2) {
-                // Couldn't parse string, assume the worst.
-                result = 1;
-            }
-        }
-        return result;
-    }
-}
-
-class AndroidLevel4PlusCompat {
-    static int getSDKInt() {
-        return android.os.Build.VERSION.SDK_INT;
-    }
+    public final static int SDK = android.os.Build.VERSION.SDK_INT;
 }
