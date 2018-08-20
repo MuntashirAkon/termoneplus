6f6e8cc751148c4577f84405a4bca509f7a805ff
Roumen Petrov <maintainer@termoneplus.com>
2018-08-20 19:54:43 +0300

    remove obsolete static AndroidCompat.V11ToV20

--- term/src/main/java/jackpal/androidterm/compat/AndroidCompat.java
+++ term/src/main/java/jackpal/androidterm/compat/AndroidCompat.java
@@ -15,11 +15,4 @@ package jackpal.androidterm.compat;
 
 public class AndroidCompat {
     public final static int SDK = android.os.Build.VERSION.SDK_INT;
-
-    // The era of Holo Design
-    public final static boolean V11ToV20;
-
-    static {
-        V11ToV20 = (SDK >= 11) && (SDK <= 20);
-    }
 }
