b6e25257dcc6b7e192c47eb6edb78ed03f24dea6
Roumen Petrov <maintainer@termoneplus.com>
2018-08-24 20:01:41 +0300

    remove obsolete atari_small drawable as current minimum is API level 9

--- emulatorview/src/main/java/jackpal/androidterm/emulatorview/Bitmap4x8FontRenderer.java
+++ emulatorview/src/main/java/jackpal/androidterm/emulatorview/Bitmap4x8FontRenderer.java
@@ -41,8 +41,7 @@ class Bitmap4x8FontRenderer extends BaseTextRenderer {
 
     public Bitmap4x8FontRenderer(Resources resources, ColorScheme scheme) {
         super(scheme);
-        int fontResource = AndroidCompat.SDK <= 3 ? R.drawable.atari_small
-                : R.drawable.atari_small_nodpi;
+        int fontResource = R.drawable.atari_small_nodpi;
         mFont = BitmapFactory.decodeResource(resources,fontResource);
         mPaint = new Paint();
         mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
