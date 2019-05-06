e719e78f7106b986af545fc96ce66ea57e6e1c92
Roumen Petrov <maintainer@termoneplus.com>
2019-05-06 19:17:40 +0300

    use theme attribute to set navigation header colors
    On Android versions before API Level 21 colors will be set by program code as attribute reference is not implemented yet in shape drawable.
    Related to ussue #1.

--- term/src/main/java/com/termoneplus/TermActionBar.java
+++ term/src/main/java/com/termoneplus/TermActionBar.java
@@ -17,6 +17,10 @@
 package com.termoneplus;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -24,6 +28,8 @@ import android.widget.Spinner;
 
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -61,6 +67,7 @@ public class TermActionBar {
         toggle.syncState();
 
         nav_view = context.findViewById(R.id.nav_view);
+        NavigationBackground.presetColors(context, nav_view);
 
         ActionBar appbar = context.getSupportActionBar();
         if (appbar != null) {
@@ -161,5 +168,57 @@ public class TermActionBar {
 
     public interface OnItemSelectedListener {
         void onItemSelected(int position);
+    }
+
+    private static class NavigationBackground {
+        private static void presetColors(AppCompatActivity context, NavigationView view) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP /* API Level 21*/) {
+                // managed as attribute in drawable header_background
+                return;
+            }
+
+            @ColorInt final int colors[] = {
+                    // see header_background
+                    0x78909C /* Blue Gray 50, 400 */,
+                    0x607D8B /* Blue Gray 50, 500 */,
+                    0x455A64 /* Blue Gray 50, 700 */
+            };
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = context.getTheme();
+            if (theme.resolveAttribute(R.attr.colorPrimaryLight, typedValue, true))
+                colors[0] = typedValue.data;
+            if (theme.resolveAttribute(R.attr.colorPrimary, typedValue, true))
+                colors[1] = typedValue.data;
+            if (theme.resolveAttribute(R.attr.colorPrimaryDark, typedValue, true))
+                colors[2] = typedValue.data;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN /* API Level 16*/)
+                Compat16.setColors(view, colors);
+            else
+                Compat1.setColors(view, colors);
+        }
+
+        @RequiresApi(16)
+        private static class Compat16 {
+            private static void setColors(NavigationView view, int[] colors) {
+                try {
+                    View header = view.getHeaderView(0);
+                    GradientDrawable drawable = (GradientDrawable) header.getBackground();
+                    drawable.setColors(colors);
+                } catch (Exception ignore) {
+                }
+            }
+        }
+
+        private static class Compat1 {
+            private static void setColors(NavigationView view, int[] colors) {
+                try {
+                    View header = view.getHeaderView(0);
+                    GradientDrawable drawable = new GradientDrawable(GradientDrawable.Orientation.TL_BR, colors);
+                    header.setBackgroundDrawable(drawable);
+                } catch (Exception ignore) {
+                }
+            }
+        }
     }
 }
