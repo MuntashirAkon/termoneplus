ab4b3e18602af78e4c520befad73e3c9add52f9f
Roumen Petrov <maintainer@termoneplus.com>
2019-08-03 10:03:44 +0300

    adaptive launcher icons

--- scripts/termoneplus-launcher-foreground.sh
+++ scripts/termoneplus-launcher-foreground.sh
@@ -4,17 +4,11 @@ set -e
 
 cd `dirname $0`
 
-for T in n r ; do
-
-case $T in
-n) XCFFILE=../docs/termoneplus-launcher-icon.xcf;;
-r) XCFFILE=../docs/termoneplus-launcher-round_icon.xcf;;
-*) exit 99;;
-esac
+XCFFILE=../docs/termoneplus-launcher-icon.xcf
 
 for MODE in l m h xh xxh xxxh ; do
   case "$MODE" in
-  l)	dpi=120;;
+  l)	dpi=120; continue;; # unused
   m)	dpi=160;;
   h)	dpi=240;;
   xh)	dpi=320;;
@@ -25,14 +19,13 @@ for MODE in l m h xh xxh xxxh ; do
 
   # launcher icon size = 32 dp * ( dpi / 160 ) * 1.5
   size=`expr $dpi \* 3 / 10`
+  xsize=`expr $dpi / 2`
+  off=`expr \( $xsize - $size \) / 2`
 
   qualifier=
   test -z "$MODE" || qualifier=-"$MODE"dpi
 
-  case $T in
-  n) PNGFILE=mipmap"$qualifier"/ic_launcher.png;;
-  r) PNGFILE=mipmap"$qualifier"/ic_launcher_round.png;;
-  esac
+  PNGFILE=mipmap"$qualifier"/ic_launcher_foreground.png
   echo creating .../$PNGFILE ... >&2
 
   # Start gimp with python-fu batch-interpreter
@@ -45,6 +38,8 @@ def convert(xcf_file, png_file):
 
     #pdb.gimp_image_convert_indexed(img, NO_DITHER, MAKE_PALETTE, 255, False, True, '');
     pdb.gimp_image_scale(img, $size, $size);
+    pdb.gimp_image_resize(img, $xsize, $xsize, $off, $off)
+    pdb.gimp_layer_resize (layer, $xsize, $xsize, $off, $off)
 
     pdb.gimp_file_save(img, layer, png_file, png_file)
     pdb.gimp_image_delete(img)
@@ -53,5 +48,4 @@ convert('$XCFFILE', '../term/src/main/res/$PNGFILE')
 
 pdb.gimp_quit(1)
 EOF
-done
 done
